f0c0811e8145b8d4e00e27543583ab83fcde6159
Ebrahim ahmed <40615465+ebrahimahmed97090@users.noreply.github.com>
2021-12-27 04:24:37 +0200

    auth

--- components/Layout/menus.js
+++ components/Layout/menus.js
@@ -1,28 +1,43 @@
 import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {autologin} from "../../store/modules/authentication/actions";
+import {
+    useDispatch,
+    useSelector
+}                     from "react-redux";
+import {
+    autologin,
+    logout
+}                     from "../../store/modules/authentication/actions";
 import socketIOClient from "socket.io-client";
-import {retrieveMeetups} from "../../store/modules/meetups/actions";
-import {Collapse, ListItemButton} from "@mui/material";
-import List from "@mui/material/List";
-import {AccountBoxOutlined, KeyOutlined, LoginOutlined, LogoutOutlined, PersonOutlined} from "@mui/icons-material";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import {retrieveMeetups}  from "../../store/modules/meetups/actions";
+import {
+    Collapse,
+    ListItemButton
+}                         from "@mui/material";
+import List               from "@mui/material/List";
+import {
+    AccountBoxOutlined,
+    KeyOutlined,
+    LoginOutlined,
+    LogoutOutlined,
+    PersonOutlined
+}                         from "@mui/icons-material";
+import ListItemIcon       from "@mui/material/ListItemIcon";
+import ListItemText       from "@mui/material/ListItemText";
 
 
 function Menus(props) {
-    const user = useSelector((state => state.authenticationReducer.user));
+    const user     = useSelector((state => state.authenticationReducer.user));
     const dispatch = useDispatch()
     useEffect(() => {
-            const ios = socketIOClient(process.env.NEXT_PUBLIC_BACK_APP_URL)
-            ios.on('fresh',
-                e => {
-                    console.log('meetupsChanged')
-                    dispatch(retrieveMeetups())
-                })
-            dispatch(autologin())
-        },
-        [])
+                  const ios = socketIOClient(process.env.NEXT_PUBLIC_BACK_APP_URL)
+                  ios.on('fresh',
+                         e => {
+                             console.log('meetupsChanged')
+                             dispatch(retrieveMeetups())
+                         })
+                  dispatch(autologin())
+              },
+              [])
     return (
         <Collapse in={props.openAuth}
                   timeout="auto"
@@ -57,21 +72,39 @@ function Menus(props) {
                     {
                         name: 'Log Out',
                         icon: <LogoutOutlined/>,
-                        link: '/authentication/logout',
+                        link: '',
                         user: true
                     },
 
                 ].map((text) => {
                     if (user) {
-                        return (text.user && <ListItemButton component="a"
-                                                             href={text.link}
-                                                             key={text.name}>
-                            <ListItemIcon>
-                                {text.icon}
-                            </ListItemIcon>
-                            <ListItemText primary={text.name}/>
-                        </ListItemButton>)
-                    } else {
+                        if (text.name !== 'Log Out') {
+                            return (text.user && <ListItemButton component="a"
+                                                                 href={text.link}
+                                                                 key={text.name}>
+                                <ListItemIcon>
+                                    {text.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={text.name}/>
+                            </ListItemButton>)
+                        }
+                        else {
+
+                            return (
+                                <ListItemButton onClick={() => dispatch(logout())}
+                                                key={text.name}>
+                                    <ListItemIcon>
+                                        {text.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text.name + 'weqw'}/>
+                                </ListItemButton>
+                            )
+
+
+                        }
+                    }
+
+                    else {
                         return (text.user === false && <ListItemButton component="a"
                                                                        href={text.link}
                                                                        key={text.name}>
--- pages/externals/carousels/swiper.js
+++ pages/externals/carousels/swiper.js
@@ -9,18 +9,15 @@ function SwiperSlider() {
     return (
         <Grid container><Grid item
                               xs={12}
-                              component={'div'}>
-            <Swiper
-
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                ...
-            </Swiper>
+                              component={'div'}><Swiper
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}>
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            ...
+        </Swiper>
         </Grid> </Grid>
     );
 }
